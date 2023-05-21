1c9953fb51444c855312c9f0910c7730415b8b07
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-21 14:57:24 +0200

    [andriiv][master] add Hardcoded Authentication

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -13,8 +13,11 @@ export default function TodoApp() {
 
 function LoginComponent() {
 
-    const [username, setUsername] = useState('andriiv');
-    const [password, setPassword] = useState('password');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
 
     function handleUsernameChange(event) {
         setUsername(event.target.value);
@@ -24,8 +27,38 @@ function LoginComponent() {
         setPassword(event.target.value);
     }
 
+    function handleSubmit() {
+        if (username === 'andriiv' && password === 'password') {
+            console.log('Success');
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+        } else {
+            console.log('Failed');
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        }
+    }
+
+    function SuccessMessageComponent() {
+        if (showSuccessMessage) {
+            return <div className="successMessage">Authenticated successfully</div>;
+        }
+        return null;
+    }
+
+    function ErrorMessageComponent() {
+        if (showErrorMessage) {
+            return <div className="errorMessage">Authenticated failed. Please check your credentials</div>;
+        }
+        return null;
+    }
+
     return (
         <div className="Login">
+
+            <SuccessMessageComponent/>
+            <ErrorMessageComponent/>
+
             <div className="LoginForm">
                 <div>
                     <label>Username</label>
@@ -36,17 +69,9 @@ function LoginComponent() {
                     <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name="login">login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>login</button>
                 </div>
             </div>
         </div>
     );
 }
-
-// function WelcomeComponent() {
-//     return (
-//         <div className="Welcome">
-//             Welcome Component
-//         </div>
-//     );
-// }
