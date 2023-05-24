8d0f6814c90d7bfa932d2c38aa94b42b59853401
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-24 14:47:22 +0200

    [andriiv] add Formik and Moment Libraries to Display Todo Component

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -24,12 +24,10 @@ export function ListTodosComponent() {
     function deleteTodo(id) {
         console.log(id);
         deleteTodoApi(username, id)
-            .then(
-                () => {
-                    setMessage(`Delete of Todo with id = ${id} successful`);
-                    refreshTodos();
-                }
-            )
+            .then(() => {
+                setMessage(`Delete of Todo with id = ${id} successful`);
+                refreshTodos();
+            })
             .catch(error => console.log(error));
     }
 
@@ -37,46 +35,40 @@ export function ListTodosComponent() {
         navigate(`/todo/${id}`);
     }
 
-    return (
-        <div className="container">
-            <h1>Things You want to do:</h1>
-            {message && <div className="alert alert-warning">{message}</div>}
-            <div>
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Is done?</th>
-                        <th>Target Date</th>
-                        <th>Delete</th>
-                        <th>Update</th>
+    return (<div className="container">
+        <h1>Things You want to do:</h1>
+        {message && <div className="alert alert-warning">{message}</div>}
+        <div>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Is done?</th>
+                    <th>Target Date</th>
+                    <th>Delete</th>
+                    <th>Update</th>
+                </tr>
+                </thead>
+                <tbody>
+                {todos.map(todo => (
+                    <tr key={todo.id}>
+                        <td>{todo.description}</td>
+                        <td>{todo.done.toString()}</td>
+                        <td>{todo.targetDate.toString()}</td>
+                        <td>
+                            <button className="btn btn-danger"
+                                    onClick={() => deleteTodo(todo.id)}>Delete
+                            </button>
+                        </td>
+                        <td>
+                            <button className="btn btn-success"
+                                    onClick={() => updateTodo(todo.id)}>Update
+                            </button>
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        todos.map(
-                            todo => (
-                                <tr key={todo.id}>
-                                    <td>{todo.description}</td>
-                                    <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toString()}</td>
-                                    <td>
-                                        <button className="btn btn-danger"
-                                                onClick={() => deleteTodo(todo.id)}>Delete
-                                        </button>
-                                    </td>
-                                    <td>
-                                        <button className="btn btn-success"
-                                                onClick={() => updateTodo(todo.id)}>Update
-                                        </button>
-                                    </td>
-                                </tr>
-                            )
-                        )
-                    }
-                    </tbody>
-                </table>
-            </div>
+                ))}
+                </tbody>
+            </table>
         </div>
-    );
+    </div>);
 }
--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,28 +2,54 @@ import {useParams} from "react-router-dom";
 import {getTodoApi} from "./api/TodoApiService";
 import {useAuth} from "./security/AuthContext";
 import {useEffect, useState} from "react";
+import {Field, Form, Formik} from "formik";
 
 export default function TodoComponent() {
 
     const {id} = useParams();
-
     const authContext = useAuth();
     const username = authContext.username;
-
     const [description, setDescription] = useState('');
+    const [targetDate, setTargetDate] = useState('');
+
     useEffect(() => getTodos(), [id]);
 
     function getTodos() {
         getTodoApi(username, id)
-            .then(response => setDescription(response.data.description))
+            .then(response => {
+                setDescription(response.data.description);
+                setTargetDate(response.data.targetDate);
+            })
             .catch(error => console.log(error));
-    };
+    }
+
+    function onSubmit(values) {
+        console.log(values);
+    }
 
     return (
         <div className="container">
             <h1>Enter Todo details</h1>
             <div>
-                description: {description}
+                <Formik initialValues={{description, targetDate}} enableReinitialize={true} onSubmit={onSubmit}>
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field type="text" className="form-control" name="description"/>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field type="date" className="form-control" name="targetDate"/>
+                                </fieldset>
+                                <div>
+                                    <button className="btn btn-success m-3" type="submit">Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
             </div>
         </div>
     );
